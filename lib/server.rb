81f37822133b53032fe4b104c22f12ff5e2cdf30
Elton Okada <elton@corp.globo.com>
2010-11-01 01:27:16 -0200

    serving a first file

--- lib/server.rb
+++ lib/server.rb
@@ -1,12 +1,16 @@
 require 'socket'
 
-server = TCPServer.open("127.0.0.1", 2911)  # Socket to listen on port 2000
+webserver = TCPServer.new("127.0.0.1", 2911)
 puts "server listening on port 2911"
 
 loop do                      
-  client = server.accept       
-  client.puts(Time.now.ctime)  
-  client.puts "Closing the connection. Bye!"
+  client = webserver.accept       
+  begin
+    displayfile = File.open('www/sample/index.html', 'r')
+    content = displayfile.read()
+    client.print content
+  rescue Errno::ENOENT
+    client.print "File not found"
+  end
   client.close                 
 end
-
