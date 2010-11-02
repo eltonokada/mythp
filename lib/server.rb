bf619c52561308cd568b2acdd5754055e498beb5
Elton Okada <elton@corp.globo.com>
2010-11-02 16:54:40 -0200

    rendering any file

--- lib/server.rb
+++ lib/server.rb
@@ -1,16 +1,16 @@
 require 'socket'
 
 webserver = TCPServer.new("127.0.0.1", 2911)
-puts "server listening on port 2911"
+puts "mythp listening on port 2911"
 
-loop do                      
-  client = webserver.accept       
+loop do
+  session = webserver.accept  
+  request = session.gets     
+  request_path = request.split[1]
   begin
-    displayfile = File.open('../www/sample/index.html', 'r')
-    content = displayfile.read()
-    client.print content
+    session.print File.open("../www/sample#{request_path}", "r").read
   rescue Errno::ENOENT
-    client.print "File not found"
+    session.print("HTTP/1.1 404/Not Found\r\nContent-type: text/html\r\n\r\n")
   end
-  client.close                 
+  session.close                 
 end
